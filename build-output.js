c76a3942a29f63f919cee51d173ece6c9d889820
Poe Bellentani <hello@poebellentani.me>
2024-06-14 01:04:21 -0300

    ++

--- build-output.js
+++ build-output.js
@@ -1,38 +1,29 @@
 import { registerTransforms } from '@tokens-studio/sd-transforms';
 import StyleDictionary from 'style-dictionary';
-import { promises } from 'fs';
 
-registerTransforms(StyleDictionary, {
-  /* options here if needed */
+// will register them on StyleDictionary object
+// that is installed as a dependency of this package.
+registerTransforms(StyleDictionary);
+
+const sd = new StyleDictionary({
+  // make sure to have source match your token files!
+  // be careful about accidentally matching your package.json or similar files that are not tokens
+  source: ['data/aplica-theme-sd/default/**/*.json'],
+  preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
+      transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
+      buildPath: 'build/css/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
 });
 
-async function run() {
-  const $themes = JSON.parse(await promises.readFile('$themes.json', 'utf-8'));
-  const configs = $themes.map(theme => ({
-    source: Object.entries(theme.selectedTokenSets)
-      .filter(([, val]) => val !== 'disabled')
-      .map(([tokenset]) => `${tokenset}.json`),
-    preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
-    platforms: {
-      css: {
-        transformGroup: 'tokens-studio',
-        transforms: ['name/kebab'],
-        files: [
-          {
-            destination: `vars-${theme.name}.css`,
-            format: 'css/variables',
-          },
-        ],
-      },
-    },
-  }));
-
-  async function cleanAndBuild(cfg) {
-    const sd = new StyleDictionary(cfg);
-    await sd.cleanAllPlatforms(); // optionally, cleanup files first..
-    await sd.buildAllPlatforms();
-  }
-  await Promise.all(configs.map(cleanAndBuild));
-}
-
-run();
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
